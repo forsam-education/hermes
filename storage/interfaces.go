3efb6984ad492f0f1f8846b2c82b8163cca702bb
QRaimbault <quentin.raimbault@gmail.com>
2019-08-01 19:15:10 +0200

    New: Handle attachement files

---
+++ storage/interfaces.go
@@ -0,0 +1,15 @@
+package storage
+
+import "io"
+
+// TemplateFetcher interface should be implemented by any service responsible to get template content from a storage manager (FS, S3 TemplateBucket, Redis... etc).
+type TemplateFetcher interface {
+	// Fetch should return the content of the template as string
+	Fetch(templateName string) (string, error)
+}
+
+// AttachementWriter interface should be implemented by any service responsible to get attachement files from a storage manager (FS, S3 TemplateBucket, Redis... etc).
+type AttachementWriter interface {
+	// Copy should, as expected, copy the attachement file to the provided io.Writer.
+	Copy(attachementPath string, writer io.Writer) error
+}
--- storageconnector/storage_connector.go
+++
@@ -1,6 +0,0 @@
-package storageconnector
-
-// StorageConnector interface should be implemented by any service responsible to get template content from a storage manager (FS, Bucket, Redis... etc).
-type StorageConnector interface {
-	GetTemplateContent(templateName string) (string, error)
-}
